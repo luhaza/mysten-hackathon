166bb81268e3c6321ec4562f0ecfb26c7f7c358d
Rick Yanashita <ry5@williams.edu>
2024-01-16 20:15:05 -0500

    react webpage

---
+++ my-app/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="page">
+      {/* <h1>wassa</h1> */}
+      <div className="flex-container">
+      <div className="flex-column"> </div>
+      <div className="flex-column">
+        <button class="button loginButton"> Sign In to Sui Wallet </button>
+      </div>
+      </div>
+      <div className="spacer"></div>
+      <MyBox />
+      <MyButton />
+    </div>
+  );
+}
+
+function MyButton() {
+  return (
+    <button>Donate</button>
+  );
+}
+
+function MyBox(){
+  return (
+    <div className="page">
+
+    </div>
+  ); 
+}
+
+export default App;
