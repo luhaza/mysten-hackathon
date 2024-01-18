94a44615326a256697d7847c63946edd46b9f58e
Christopher Brown <cjb4@williams.edu>
2024-01-17 21:49:47 -0500

    save

---
+++ app-lts/src/components/CreditsButton.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './CreditsButton.css';
+
+interface CreditsButtonProps {
+    credits : number;
+}
+
+const CreditsButton : React.FC<CreditsButtonProps>= ({ credits }) => {
+    const [showCredits, setShowCredits] = useState(true);
+
+    const toggleCredits = () => {
+        setShowCredits(!showCredits);
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-center text-white font-bold">
+            
+            <button onClick={toggleCredits} className="bg-blue-400 hover:bg-blue-700 text-white font-bold py-10% px-5% rounded m-4 sm:text-md md:text-lg lg:text-xl">
+                Show Credits
+            </button>
+            {showCredits && (
+                <div className="credits-container" style={{display:'block'}}>
+                    <h3>Credits: ${credits}</h3>
+                </div>
+            )}
+
+        </div>
+    )
+}
+
+export default CreditsButton;
---
+++ app-lts/src/components/UserAndRewards.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../App.css';
+import RewardsList from "./RewardsList"
+import UserProfile from "./UserProfile"
+
+const UserAndRewards = () => {
+    return (
+        <div className="flex-column flex items-center justify-center h-screen bg-blue-200">
+            <UserProfile/>
+            <RewardsList/>
+        </div>
+    )
+}
+
+export default UserAndRewards;
