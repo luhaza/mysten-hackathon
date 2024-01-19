1c3ddf384d52ca9094fd0b88d2c481eb1ed672a9
Christopher Brown <cjb4@williams.edu>
2024-01-18 22:27:52 -0500

    user can specify donation amount now

--- app-lts/src/components/DonateButton.tsx
+++ app-lts/src/components/DonateButton.tsx
@@ -5,14 +5,14 @@ import {
 import React, { useState } from 'react';
 import { TransactionBlock } from "@mysten/sui.js/transactions"; 
 
-const donateAmount : number = 1000;
+// const donateAmount : number = 1000;
 interface DonateButtonProps {
     org_id : string
 }
 
 const DonateButton : React.FC<DonateButtonProps>= ({ org_id }) => {
 	const { mutate: signAndExecuteTransactionBlock } = useSignAndExecuteTransactionBlock();
-	const [digest, setDigest] = useState('');
+    const [donateAmount, setDonateAmount] = useState(1000); // Default donate amount
 	const currentAccount = useCurrentAccount();
  
     const donate = () => {
@@ -34,31 +34,42 @@ const DonateButton : React.FC<DonateButtonProps>= ({ org_id }) => {
 
 			{currentAccount && (
 				<>
-					<div>
-						<button
-							onClick={() => {
-								signAndExecuteTransactionBlock(
-									{
-										transactionBlock: donate(),
-										chain: 'sui:testnet',
-									},
-									{
-										onSuccess: (result) => {
-											console.log('executed transaction block', result);
-											setDigest(result.digest);
-										},
-                                        onError: (res) => {
-                                            console.log(res);
-                                        }
-									},
-								);
-							}}
+                    <div className="flex">
+                        <button className="bg-blue-500 rounded 5px">
+                        <label htmlFor="donateAmount">Donate Amount: </label>
+                        <input
+                        type="number"
+                        id="donateAmount"
+                        value={donateAmount}
+                        onChange={(e) => setDonateAmount(parseInt(e.target.value, 10))}
+                        />
+                        </button>
+                        
+                    
+                        <div>
+                            <button
+                                onClick={() => {
+                                    signAndExecuteTransactionBlock(
+                                        {
+                                            transactionBlock: donate(),
+                                            chain: 'sui:testnet',
+                                        },
+                                        {
+                                            onSuccess: (result) => {
+                                                console.log('executed transaction block', result);
+                                            },
+                                            onError: (res) => {
+                                                console.log(res);
+                                            }
+                                        },
+                                    );
+                                }}
 
-						className="bg-blue-400 hover:bg-blue-700 text-white font-bold rounded-lg py-10% px-5% rounded m-4 sm:text-md md:text-lg lg:text-xl">
-							DONATE
-						</button>
-					</div>
-                    <div>Digest: {digest}</div>
+                            className="bg-blue-400 hover:bg-blue-700 text-white font-bold rounded-lg py-10% px-5% rounded m-4 sm:text-md md:text-lg lg:text-xl">
+                                DONATE
+                            </button>
+                        </div>
+                    </div>
 				</>
 			)}
 		</div>
